6c682700bbbc1cba3f5053df8895d63bec7e0422
Caprena Richie <robcaprena@gmail.com>
2021-10-08 15:59:54 -0400

    made a correction after John looked over

--- ClassesExercise/Program.cs
+++ ClassesExercise/Program.cs
@@ -2,15 +2,13 @@
 
 namespace ClassesExercise
 {
-    class Car
+    class Program
     {
-        public string Make { get; set; }
-        public string Model { get; set; }
-        public int Year { get; set; }
+        
         public static void Main(string[] args)
         {
             var car1 = new Car();
-            //Car car1 = new Car("Mazda", "Miata", 2014);
+            
 
             car1.Make = "Mazda";
             car1.Model = "Miata";
